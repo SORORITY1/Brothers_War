3f2896153b88acbfb54edd346ac0d10c296ec1e5
SORORITY1 <150854326+SORORITY1@users.noreply.github.com>
2023-11-14 12:37:24 -0800

    Create anti_psychosis_war.php

---
+++ anti_psychosis_war.php
@@ -0,0 +1,9 @@
+<?php
+
+$file = fopen("dystopian_war.txt", "w");
+
+fwrite($file, "Societerian and dystopian war amongst brothers focused on written dystopian novels such as 1984.");
+
+fclose($file);
+
+?>
